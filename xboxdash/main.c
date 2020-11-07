2d95cf62d97aa1155179e83f604d91e50550c2a3
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2020-11-07 11:53:27 -0800

    Update main.c
    cleaning up.

--- xboxdash/main.c
+++ xboxdash/main.c
@@ -1,28 +1,21 @@
-//#include <xboxrt/debug.h>
-//#include <windows.h>
+#include <windows.h>
 #include <hal/debug.h>
 #include <hal/video.h>
 #include <pbkit/pbkit.h>
 #include <hal/xbox.h>
-#include "stdio.h"
+#include <stdio.h>
 
 void main(void)
 {
-    int i;
-
     XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
-
     if (pb_init()) {
         Sleep(2000);
         XReboot();
         return;
     }
-
     pb_show_debug_screen();
     debugPrint("Please insert an Xbox disc and restart the system...\n");
-    while (1) {
-        Sleep(1000);
-    }
+    while (1) Sleep(1000);
     pb_kill();
     XReboot();
 }
