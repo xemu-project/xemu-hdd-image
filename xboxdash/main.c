7ed122f9dbd80b0c66ebe3eb50e6ab2c4f0c7b58
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2020-11-07 12:10:16 -0800

    da fifth update to main.c
    we're beginning to reach peak power here.

--- xboxdash/main.c
+++ xboxdash/main.c
@@ -1,12 +1,10 @@
 #include <windows.h>
 #include <hal/debug.h>
 #include <hal/video.h>
-#include <hal/xbox.h>
 
 void main(void)
 {
-    XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
-    debugPrint("Please insert an Xbox disc and restart the system...\n");
+    XVideoSetMode(640, 480, 32, REFRESH_DEFAULT); 
+    debugPrint("Please insert an Xbox disc and restart the system...\n"); 
     while (1) Sleep(1000);
-    XReboot();
 }
