3f7552768a9274fb39fe067733dcd6f14e543fae
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2020-11-07 12:06:10 -0800

    da fourth update to main.c
    wow, this shrunk a considerable amount.

--- xboxdash/main.c
+++ xboxdash/main.c
@@ -1,20 +1,12 @@
 #include <windows.h>
 #include <hal/debug.h>
 #include <hal/video.h>
-#include <pbkit/pbkit.h>
 #include <hal/xbox.h>
 
 void main(void)
 {
     XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
-    if (pb_init()) {
-        Sleep(2000);
-        XReboot();
-        return;
-    }
-    pb_show_debug_screen();
     debugPrint("Please insert an Xbox disc and restart the system...\n");
     while (1) Sleep(1000);
-    pb_kill();
     XReboot();
 }
