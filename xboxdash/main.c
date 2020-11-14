95f0b01699f350ecf419a40fc69b0f30cec6a598
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2020-11-13 16:29:24 -0800

    rework some things

--- xboxdash/main.c
+++ xboxdash/main.c
@@ -12,15 +12,30 @@ void main(void)
     debugPrint("Please insert a disc...\n");
     for (;;) {
         HalReadSMCTrayState(&status, NULL);
-        if (status == 16) { // if the drive status is 0x10, wait for a disc to be inserted.
-            debugPrint("Tray is Open. Waiting for a disc.\n", status, oldstatus);
-            Sleep(1000);
-            debugClearScreen();
-        }
-        if (status == 96) { // we've got a disc, drive status is 0x60, lets try to launch default.xbe
-            debugPrint("You've got games on this disc? Attempting to launch...\n", status, oldstatus);
-            XLaunchXBE("\\Device\\CdRom0\\default.xbe"); // if the screen keeps flickering with the same text as before, double check that you packed your iso correctly, we cant find default.xbe.
-            Sleep(2000);
+        switch (status)//Using a switch now, i think this just looks cooler. if statements work fine i guess idk.
+        {
+        case 16 : //if satus is 16, we know the tray is open
+            if (oldstatus != status)
+            {
+                debugPrint("Tray is Open. Waiting for a disc.\n", status, oldstatus);
+                oldstatus = status; // very stupid solution do a problem that is probably very easy to fix but my pea-sized brain cant figure it out.
+            }
+            break;
+        case 96 : //if status is 96, we've got something in the tray and are going to try and launch it, 3 times. If it fails launching, then no default.xbe was found.
+            debugPrint("We've got a disc, attempting to launch!\n");
+            for (int i = 0; i < 3; i++)
+            {
+                if (oldstatus != status)
+                {
+                    XLaunchXBE("\\Device\\CdRom0\\default.xbe"); // if the screen keeps flickering with the same text as before, double check that you packed your iso correctly, we cant find default.xbe.
+                    if (i == 3)
+                    {
+                        debugPrint("Could not launch default.xbe! Please check if your disc is scratched, or packed incorrectly.");
+                        oldstatus = status;
+                    }
+                    break;
+                }
+            }
         }
         Sleep(200);
     }
